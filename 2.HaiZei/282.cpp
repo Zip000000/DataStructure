43c8de4c7a67aef7ffc2e714f519c2de9feedbb2
Zip <307110017@qq.com>
2019-08-22 23:52:17 +0800

    practice UnionSet

--- 2.HaiZei/282.cpp
+++ 2.HaiZei/282.cpp
@@ -26,37 +26,42 @@ int cnt = 1, root = 1;
 int arr[(MAX_N >> 5) + 5];
 inline int getNewNode() { return ++cnt; }
 
-void insert(int n) {
+void insert(int num) {
     int p = root;
-    for (int i = BASE; i >= 0; --i) {
-        int ind = !!((1 << i) & n);
+    for (int i = BASE; i >= 0; i--) {
+        int ind = !!((1 << i) & num);
         if (Node[p].next[ind] == 0) Node[p].next[ind] = getNewNode();
         p = Node[p].next[ind];
     }
-    Node[p].num = n;
+    Node[p].num = num;
     return;
 }
 
-int query(int n) {
-    int rp = root;
-    for (int i = BASE; i >= 0; --i) {
-        int ind = !!((1 << i));
-        int rnxt = Node[rp].next[!ind];
-        int nxt = Node[rp].next[ind];
-        if (rnxt == 0) rp = rnxt; else rp = nxt;
+int query(int num) {
+    int p = root;
+    for (int i = BASE; i >= 0; i--) {
+        int ind = !!((1 << i) & num);
+        int rind = !ind;
+        if (Node[p].next[rind]) p = Node[p].next[rind];
+        else p = Node[p].next[ind];
     }
-    return n ^ Node[rp].num;
+    return num ^ Node[p].num;
 }
 
 int main() {
     int n;
     cin >> n;
     for (int i = 0; i < n; i++) cin >> arr[i], insert(arr[i]);
-    int max = 0;
+    int ans = 0;
     for (int i = 0; i < n; i++) {
-        int tmp = query(arr[i]);
-        if (tmp > max) max = tmp;
+        ans = max(ans, query(arr[i]));
     }
-    cout << max << endl;
+    cout << ans << endl;
     return 0;
 }
+
+        /*
+        int rnxt = Node[rp].next[!ind];
+        int nxt = Node[rp].next[ind];
+        if (nxt == 0) rp = rnxt; else rp = nxt;
+        */
