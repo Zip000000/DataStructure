82e46d57aac0ef64cf7ca46ca51e7752a945e674
Zip <307110017@qq.com>
2019-08-25 18:00:40 +0800

    BiTrieHaff Project

--- 4.BiTrieHaff/1.Haffman.c
+++ 4.BiTrieHaff/1.Haffman.c
@@ -7,6 +7,7 @@
 #include<stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
 
 #define MAX_N 100
 #define MAX_LEN 1024
@@ -16,7 +17,7 @@
     __typeof(a) __tmp = a; \
     a = b, b = __tmp; \
 }
-
+/*
 typedef struct Words {
     unsigned char *str;
 } Words;
@@ -26,14 +27,13 @@ typedef struct ALLWords {
     int n;
 } ALLWords;
 ALLWords allwords;
-
+*/
 typedef struct Node {
     unsigned char ch;
     int p;
     struct Node *l, *r;
 } Node;
 Node *nodes[MAX_NODE + 5];
-int nodecnt = 0;
 
 typedef struct Code {
     unsigned char ch;
@@ -41,40 +41,53 @@ typedef struct Code {
 } Code;
 
 typedef struct HaffmanTree {
-    Node *root;
     int n;
+    Node *root;
     Code *codes;
 } HaffmanTree;
-
+/*
 int get_all_words(Words *word) {
     unsigned char buff[MAX_LEN + 5];
     int cnt = 0;
     while (~scanf("%s", buff)) word[cnt++].str = strdup(buff);
     return cnt;
 }
+*/
 
 Node *getNewNode() {
     Node *n = (Node *)malloc(sizeof(Node));
+    n->ch = 0;
+    n->p = 0;
+    n->l = n->r = NULL;
     return n;
 }
 
-void get_words_frequency(ALLWords *all, Node **nodes) {
-    int n = all->n;
-    for (int i = 0; i < n; i++) {
-        unsigned char *s = all->word[i].str;
-        for (int j = 0; s[j]; j++) {
-            if (nodes[s[j]] == NULL) {
-                nodes[s[j]] = getNewNode();
-                nodes[s[j]]->ch = s[j];
-                nodecnt++;
-            } 
-            nodes[s[j]]->p += 1;
-        }
+void outputNodes(Node **nodes) {
+    for (int i = 0; i < MAX_NODE; i++) {
+        if (nodes[i] == NULL) continue;
+        printf("%d : ch = %d : p = %d \n", i, nodes[i]->ch, nodes[i]->p);
     }
 }
 
+Node **get_words_frequency() {
+    Node **nodes = (Node **)malloc(sizeof(Node *) * (MAX_NODE + 5));
+    unsigned char ch;
+    while (~scanf("%c", &ch)) {
+        if (nodes[ch] == NULL) nodes[ch] = getNewNode();
+        nodes[ch]->ch = ch;
+        nodes[ch]->p += 1;
+    }
+    for (int i = 0; i < MAX_NODE; i++) {
+        if (nodes[i] != NULL) continue;
+        nodes[i] = getNewNode();
+        nodes[i]->ch = i;
+    }
+    outputNodes(nodes);
+    return nodes;
+}
+
 Node *getCombineNode(Node *node1, Node *node2) {
-    Node *n = (Node *)malloc(sizeof(Node));
+    Node *n = getNewNode();
     n->p = node1->p + node2->p;
     n->l = node1;
     n->r = node2;
@@ -87,50 +100,80 @@ HaffmanTree *getNewTree() {
     return t;
 }
 
+void insertOnce(Node**nodes, int i) {
+    for (int j = i; j >= 1; j--) {
+        if (nodes[j]->p <= nodes[j - 1]->p) continue;
+        swap(nodes[j], nodes[j - 1]);
+    }
+}
+void extractCode(Node *root, Code *codes, int k, char *buff) {
+    buff[k] = '\0';
+    if (root->l == NULL && root->r == NULL) {
+        codes[root->ch].ch = root->ch;
+        codes[root->ch].code = strdup(buff);
+        printf("ch = %d code : %s\n", codes[root->ch].ch, codes[root->ch].code);
+        return ;
+    }
+    buff[k] = '0';
+    extractCode(root->l, codes, k + 1, buff);
+    buff[k] = '1';
+    extractCode(root->r, codes, k + 1, buff);
+    return;
+}
 HaffmanTree *build(Node **nodes) {
     HaffmanTree *tree = getNewTree();
     for (int i = 1; i < MAX_NODE; i++) {
-        for (int j = i; j >= 1; j--) {
-            if (nodes[j] == NULL) {
-                continue;
-            } else if (nodes[j - 1] == NULL) {
-                swap(nodes[j], nodes[j - 1]);
-                continue;
-            } else {
-                if (nodes[j]->p <= nodes[j - 1]->p) break;
-                swap(nodes[j], nodes[j - 1]);
-            }
-        }
+        insertOnce(nodes, i);
     }
+    printf("after sort\n");
+    outputNodes(nodes);
     /*
     for (int i = 0; nodes[i]; i++) {
         printf("%d : %c : p = %d\n", i, nodes[i]->ch, nodes[i]->p);
     }
     */
-    for (int i = nodecnt - 1; i >= 1; --i) {
-        Node *n = getCombineNode(nodes[i], nodes[i - 1]);
-        nodes[i - 1] = n;
-        for (int j = i - 1; j >= 1; --j) {
-            if (nodes[j]->p <= nodes[j - 1]->p) break;
-            swap(nodes[j], nodes[j - 1]);
-        }
+    for (int i = MAX_NODE - 1; i >= 1; --i) {
+        Node *new_node = getCombineNode(nodes[i], nodes[i - 1]);
+        nodes[i - 1] = new_node;
+        insertOnce(nodes, i - 1);
     }
     tree->root = nodes[0];
-    tree->n = nodecnt;
+    tree->n = MAX_NODE;
+    char *buff = (char *)calloc(sizeof(char), MAX_NODE);
+    extractCode(tree->root, tree->codes, 0, buff);
+    printf("after extract Code\n");
     free(nodes);
     return tree;
 }
 
+void write_down_Haffman_codes(Code *codes, const char *codesfile) {
+    FILE *fp = fopen("./Codes", "w");
+    if (fp == NULL) { perror("fopen in write_down_Haffman_codes"); exit(1); }
+    for (int i = 0; i < MAX_NODE; i++) {
+        fprintf(fp, "%d %s\n", codes[i].ch, codes[i].code);
+    }
+    fclose(fp);
+    return ;
+}
+
 int main() {
-    allwords.n = get_all_words(allwords.word);
-    printf("after get all words\n");
-    get_words_frequency(&allwords, nodes);
-    printf("after get_word_frequency\n");
+    char codesfile[100] = "./Codes";
+    FILE *fp = fopen(codesfile, "r");
+    if (fp != NULL) {
+        printf("Codes文件已存在\n"); 
+        fclose(fp); 
+        exit(1);
+    } else {
+        printf("开始获得当前环境下的字符最优编码......\n");
+    }
+    Node **nodes = get_words_frequency();
+    printf("词频统计完毕\n");
     
     HaffmanTree *tree = build(nodes);
-
-    extractCode(tree,)
-
+    printf("Huffman树建立完毕\n");
+    write_down_Haffman_codes(tree->codes, codesfile);
+    printf("Codes文件生成完毕\n");
+    
     return 0;
 }
 
@@ -138,24 +181,14 @@ int main() {
 
 
 
-
-
-/*
-    for (int i = 0; i < cnt; i++) {
-        printf(" %d : %s\n", i, word[i].str);
-    }
-  */  
-    
-    
-    
     /*
-    for (int i = 0; i < MAX_NODE; i++) {
-        printf("%d : p = %d\n", node[i].ch, node[i].p);
+    for (int j = i; j >= 1; j--) {
+        if (nodes[j] == NULL) {
+            break;
+        } else if (nodes[j - 1] == NULL || nodes[j]->p > nodes[j - 1]->p) {
+            swap(nodes[j], nodes[j - 1]);
+            continue;
+        }
+        break;
     }
-
-
-    for (int i = 0; i < MAX_NODE; i++) {
-        printf("%d %c : %d\n", i, nodes[i].ch, nodes[i].p);
-    }
-    printf(" num = %d\n", nodecnt);
     */
--- 4.BiTrieHaff/2.BiTrie.c
+++ 4.BiTrieHaff/2.BiTrie.c
@@ -1,21 +1,27 @@
 /*************************************************************************
-	> File Name: test.c
+	> File Name: 2.BiTrie.c
 	> Author: Zip 
 	> Mail: 307110017@qq.com 
-	> Created Time: 2019年08月24日 星期六 10时06分16秒
+	> Created Time: 2019年08月25日 星期日 15时56分40秒
  ************************************************************************/
-
 #include<stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include "1.Haffman.c"
+
+typedef struct Trie {
+    int flag;
+    struct Trie *l, *r;
+} Trie;
 
 int main() {
-    unsigned char str;
     
-    while (~scanf("%c", &str)) {
-        printf("%d", str);
-        printf("%c", str);
-    }
-
+    
+    
+    
+    
+    
     return 0;
 }
+
+
